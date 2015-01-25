4b0e0655bb4010714773e87a34603841c2e2085a
katrin-polyakova <katrin21@ukr.net>
2015-01-25 13:45:52 +0200

    new borsch

--- PlayWithArraysTests/PlayWithArraysTests.m
+++ PlayWithArraysTests/PlayWithArraysTests.m
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import "NSArray+Testutils.h"
 
 @interface PlayWithArraysTests : XCTestCase
 
@@ -56,7 +57,7 @@
 }
 
 - (void)testExample02 {
-    NSArray *borsh = @[@"Картошка", @"Лук", @"Морковка", @"Свекла", @"Капуста", @"Чеснок", @"Томат"];
+    NSArray *borsh = [self getTestData];
 
     for(int i=borsh.count-1; i>=0; i--){
         NSString *string = borsh[(NSUInteger)i];
@@ -67,19 +68,24 @@
     for (NSString * item in borsh.reverseObjectEnumerator){
         NSLog(@"%@", item);
     }
+    NSLog(@"\n");
 
     [borsh.reverseObjectEnumerator.allObjects enumerateObjectsUsingBlock:^(NSString *item, NSUInteger idx, BOOL *stop) {
         NSLog(@"%@", item);
 
-        if (idx == 1) {
+        if (idx == 2) {
             *stop = YES;
         }
     }];
 
 }
 
+- (NSArray *)getTestData {
+    return [NSArray loadTestListByName:@"Vegetables" clazz:[PlayWithArraysTests class]];
+}
+
 - (void)testExample03 {
-    NSArray *borsh = @[@"Картошка", @"Лук", @"Морковка", @"Свекла", @"Капуста", @"Чеснок", @"Томат"];
+     NSArray *borsh = [self getTestData];
 
     int i = [borsh indexOfObject: @"Лук"];
 //    int i = [borsh indexOfObject: [NSString stringWithFormat:@"Лу", @"к"]];
@@ -88,7 +94,7 @@
 }
 
 - (void)testExample04 {
-    NSArray *borsh = @[@"Картошка",@"Перец", @"Лук", @"Морковка", @"Свекла", @"Капуста", @"Чеснок", @"Томат"];
+    NSArray *borsh = [self getTestData];
 
     int i = [borsh indexOfObjectPassingTest:^BOOL(NSString *item , NSUInteger idx, BOOL *stop) {
         BOOL result = [item hasPrefix:@"М"];
@@ -98,14 +104,24 @@
 }
 
 - (void)testExample05 {
-    NSArray *borsh = @[@"Картошка", @"Томат",@"Перец", @"Лук", @"Морковка", @"Свекла", @"Капуста", @"Чеснок"];
+    NSArray *borsh = [self getTestData];
+
+    NSString *firstPlacePrefix = @"Т";
 
     borsh = [borsh sortedArrayUsingComparator:^NSComparisonResult(NSString * obj1, NSString * obj2) {
-        return [obj1 compare: obj2];
+        if([obj1 hasPrefix:firstPlacePrefix] && [obj2 hasPrefix:firstPlacePrefix]){
+            return [obj1 compare:obj2];
+        } else if ([obj2 hasPrefix:firstPlacePrefix]){
+            return NSOrderedDescending;
+        } else if ([obj1 hasPrefix:firstPlacePrefix]){
+            return NSOrderedAscending;
+        } else {
+            return [obj1 compare: obj2];
+        }
+
     }];
 
 //    borsh = [borsh sortedArrayUsingComparator:^NSComparisonResult(NSString * obj1, NSString * obj2) {
-//
 //        NSComparisonResult result = [obj1 compare: obj2];
 //
 //        if ([obj1 hasPrefix:@"Ч"]) {
@@ -114,10 +130,102 @@
 //        return result;
 //    }];
 
-    for (NSString * item in borsh){
-        NSLog(@"%@", item);
+    [borsh logArray];
+
+//    for (NSString * item in borsh){
+//        NSLog(@"%@", item);
+//    }
+
+}
+
+- (void)testExample06 {
+    NSArray *borsh = [self getTestData];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF BEGINSWITH %@", @"Т"];
+    NSArray *filtered = [borsh filteredArrayUsingPredicate:predicate];
+
+    [filtered logArray];
+}
+
+- (void)testExamples07 {
+    NSArray *borsh = [self getTestData];
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString * evaluatedObject, NSDictionary *bindings) {
+
+        NSRange range = [evaluatedObject rangeOfString:@"ка" options:NSCaseInsensitiveSearch];
+
+        BOOL result = range.location != NSNotFound;
+
+        return result;
+    }];
+    NSArray *filtered = [borsh filteredArrayUsingPredicate:predicate];
+
+    [filtered logArray];
+}
+
+- (void)testExample08 {
+    NSArray *borsh = [self getTestData];
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString * evaluatedObject, NSDictionary *bindings) {
+        BOOL result = evaluatedObject.length > 5;
+        return result;
+    }];
+    NSArray *filtered = [borsh filteredArrayUsingPredicate:predicate];
+
+    [filtered logArray];
+}
+
+- (void)testExample09 {
+    NSArray *borsh = [self getTestData];
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString * evaluatedObject, NSDictionary *bindings) {
+        BOOL result = NO;
+        if ([evaluatedObject hasPrefix:@"К"] && [evaluatedObject hasSuffix:@"а"]){
+            result = YES;
+        }
+        return result;
+    }];
+    NSArray *filtered = [borsh filteredArrayUsingPredicate:predicate];
+
+    [filtered logArray];
+}
+
+- (void)testExample10 {
+    NSArray *borsh = [self getTestData];
+    NSMutableArray *result = [NSMutableArray array];
+
+    for(NSUInteger i=0; i<borsh.count; i++){
+        NSString *string = borsh[i];
+        NSString * replaced = [string stringByReplacingOccurrencesOfString:@"а" withString:@"о"];
+        replaced = [replaced stringByReplacingOccurrencesOfString:@"А" withString:@"О"];
+        replaced = [replaced stringByReplacingOccurrencesOfString:@"у" withString:@"ю"];
+        replaced = [replaced stringByReplacingOccurrencesOfString:@"ы" withString:@"ю"];
+        [result addObject:replaced];
     }
 
+    [result logArray];
+}
+
+- (void)testExample11 {
+    NSArray *borsh = [self getTestData];
+    NSMutableArray *filteredArray = [NSMutableArray array];
+    [borsh enumerateObjectsUsingBlock:^(NSString *item, NSUInteger idx, BOOL *stop) {
+        NSRange range = [item rangeOfString:@"ка" options:NSCaseInsensitiveSearch];
+        if (range.location != NSNotFound) {
+            [filteredArray addObject:item];
+        }
+    }];
+
+    [filteredArray logArray];
+}
+
+- (void)testExample12 {
+    NSArray *borsh = [self getTestData];
+    NSMutableArray *filteredArray = [borsh mutableCopy];
+    [borsh enumerateObjectsUsingBlock:^(NSString *item, NSUInteger idx, BOOL *stop) {
+        NSRange range = [item rangeOfString:@"ка" options:NSCaseInsensitiveSearch];
+        if (range.location == NSNotFound) {
+            [filteredArray removeObject:item];
+        }
+    }];
+
+    [filteredArray logArray];
 }
 
 - (void)testPerformanceExample {
